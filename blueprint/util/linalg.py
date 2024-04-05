4c1a2d1f4bd4881ad6191c807ad1f2e622b137aa
Boris Varbanov <b.m.varbanov@gmail.com>
2024-04-04 20:36:03 -0400

    Update linalg.py
    Small changes in syntax

--- blueprint/util/linalg.py
+++ blueprint/util/linalg.py
@@ -1,3 +1,5 @@
+"""Module for linear algebra utilities."""
+
 from typing import Iterable, List
 
 from collections import deque
@@ -8,7 +10,7 @@ from jax import numpy as jnp
 from jax import Array
 
 
-def transform_op(op: Array, trans_mat: Array) -> Array:
+def transform_op(op: Array, trans_op: Array) -> Array:
     """
     transform_op Transforms an operator to a new basis.
 
@@ -24,8 +26,9 @@ def transform_op(op: Array, trans_mat: Array) -> Array:
     Array
         The transformed operator.
     """
-    trans_op = jnp.conj(trans_mat).T @ op @ trans_mat
-    return trans_op
+    transformed_op = jnp.conj(trans_op).T @ op @ trans_op
+    return transformed_op
+
 
 def tensor_product(ops: Iterable[Array]) -> Array:
     """
@@ -43,10 +46,11 @@ def tensor_product(ops: Iterable[Array]) -> Array:
     """
     return reduce(jnp.kron, ops)
 
+
 def embed_op(
     op: Array,
     ind: int,
-    qubit_dims: Iterable[int],
+    dims: Iterable[int],
 ) -> Array:
     """
     embed_operator Embeds an operator into a larger Hilbert space.
@@ -57,7 +61,7 @@ def embed_op(
         The operator to embed.
     ind : int
         The index of the qubit in the larger Hilbert space.
-    qubit_dim : int
+    dims : int
         The dimension of the qubit in the larger Hilbert space.
 
     Returns
@@ -65,16 +69,14 @@ def embed_op(
     Array
         The embedded operator.
     """
-    expanded_ops = (
-        op if i == ind else jnp.identity(dim) for i, dim in enumerate(qubit_dims)
-    )
+    expanded_ops = (op if i == ind else jnp.identity(dim) for i, dim in enumerate(dims))
     return tensor_product(expanded_ops)
 
 
 def embed_ops(
     ops: List[Array],
     inds: List[int],
-    qubit_dims: Iterable[int],
+    dims: Iterable[int],
 ) -> Array:
     """
     embed_ops Embeds operators into a larger Hilbert space.
@@ -85,7 +87,7 @@ def embed_ops(
         The operators to embed.
     inds : Iterable[int]
         The index of the qubit in the larger Hilbert space.
-    qubit_dims : Iterable[int]
+    dims : Iterable[int]
         The dimensions of the qubits in the larger Hilbert space.
 
     Returns
@@ -95,7 +97,7 @@ def embed_ops(
     """
     expanded_ops = deque()
 
-    for ind, dim in enumerate(qubit_dims):
+    for ind, dim in enumerate(dims):
         if ind in inds:
             idx = inds.index(ind)
             expanded_ops.append(ops[idx])
