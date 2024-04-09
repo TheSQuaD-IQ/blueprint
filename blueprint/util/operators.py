22a658638615a8e384d5fc93dcd726e8e4e82909
Boris Varbanov <b.m.varbanov@gmail.com>
2024-04-08 23:12:57 -0400

    Update to basis and pauli ops

--- blueprint/util/operators.py
+++ blueprint/util/operators.py
@@ -1,10 +1,13 @@
+import math
 from typing import Tuple, Any
 
 from jax import Array
 from jax import numpy as jnp
 
 
-def get_pauli_ops(dtype: Any | None = None) -> Tuple[Array, Array, Array, Array]:
+def get_pauli_ops(
+    normalize: bool = False, dtype: Any | None = None
+) -> Tuple[Array, Array, Array, Array]:
     """
     get_pauli_ops Returns the qubit Pauli operators, more specifically in the order of I, X, Y, Z.
 
@@ -21,10 +24,12 @@ def get_pauli_ops(dtype: Any | None = None) -> Tuple[Array, Array, Array, Array]
     else:
         dtype = jnp.complex64
 
-    pauli_i = jnp.array([[1, 0], [0, 1]], dtype=dtype)
-    pauli_x = jnp.array([[0, 1], [1, 0]], dtype=dtype)
-    pauli_y = jnp.array([[0, -1j], [1j, 0]], dtype=dtype)
-    pauli_z = jnp.array([[1, 0], [0, -1]], dtype=dtype)
+    prefactor = 1 / math.sqrt(2) if normalize else 1
+
+    pauli_i = prefactor * jnp.array([[1, 0], [0, 1]], dtype=dtype)
+    pauli_x = prefactor * jnp.array([[0, 1], [1, 0]], dtype=dtype)
+    pauli_y = prefactor * jnp.array([[0, -1j], [1j, 0]], dtype=dtype)
+    pauli_z = prefactor * jnp.array([[1, 0], [0, -1]], dtype=dtype)
 
     # NOTE: could be a dictionary instead, but this seems more useful.
     return pauli_i, pauli_x, pauli_y, pauli_z
