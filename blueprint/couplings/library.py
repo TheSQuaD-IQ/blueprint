9bbab9001a2646531691e654c60842c61e089e97
Boris Varbanov <b.m.varbanov@gmail.com>
2024-05-01 11:59:28 -0400

    docstring fix

--- blueprint/couplings/library.py
+++ blueprint/couplings/library.py
@@ -8,23 +8,23 @@ Numeric = Union[float, complex]
 
 def get_capacitive_coupling(
     label: str,
-    qubit: QuantumSystem,
-    coupled_qubit: QuantumSystem,
+    system: QuantumSystem,
+    coupled_system: QuantumSystem,
     coupling_strength: Numeric,
 ) -> Coupling:
     """
-    get_capacitive_coupling Returns a capacitive coupling between two qubits.
+    get_capacitive_coupling Returns a capacitive coupling between two systems (qubits or resonators).
     This corresponds to a term of the form $H_{coup} = \\eta \\hat{n}_{1}\\hat{n}_{2}$,
     where $\\eta$ corresponds to the coupling strength (`coupling_strength`),
     and $\\hat{n}_{1}$ and $\\hat{n}_{2}$ are the charge operators of the
-    two qubits (`qubit` and `coupled_qubit`).
+    two systems (`system` and `coupled_system`).
 
     Parameters
     ----------
-    qubit : QuantumSystem
-        The first qubit of the pair that are coupled.
-    coupled_qubit : QuantumSystem
-        The second qubit of the pair that are coupled.
+    system : QuantumSystem
+        The first quantum system of the pair that are coupled.
+    coupled_system : QuantumSystem
+        The second quantum system of the pair that are coupled.
     label : str
         The label of the coupling term.
     coupling_strength : Numeric
@@ -33,27 +33,29 @@ def get_capacitive_coupling(
     Returns
     -------
     Coupling
-        The capacitive coupling between the two qubits.
+        The capacitive coupling between the two systems.
 
     Raises
     ------
     AttributeError
-        If the qubit does not have a charge operator.
+        If the system does not have a charge operator.
     AttributeError
-        If the coupled_qubit does not have a charge operator.
+        If the coupled_system does not have a charge operator.
     """
     try:
-        qubit_op = qubit.get_charge_op()
-    except AttributeError as exc:
-        raise AttributeError(f"Qubit {qubit} does not have a charge operator.") from exc
-
-    try:
-        coupled_op = coupled_qubit.get_charge_op()
+        system_op = system.get_charge_op()
     except AttributeError as exc:
         raise AttributeError(
-            f"Qubit {coupled_qubit} does not have a charge operator."
+            f"system {system} does not have a charge operator."
         ) from exc
-    operator = qubit_op @ coupled_op
-    qubit_labels = (qubit.label, coupled_qubit.label)
-    coupling = Coupling(label, operator, coupling_strength, qubit_labels)
+
+    try:
+        coupled_op = coupled_system.get_charge_op()
+    except AttributeError as exc:
+        raise AttributeError(
+            f"system {coupled_system} does not have a charge operator."
+        ) from exc
+    operator = system_op @ coupled_op
+    system_labels = (system.label, coupled_system.label)
+    coupling = Coupling(label, operator, coupling_strength, system_labels)
     return coupling
